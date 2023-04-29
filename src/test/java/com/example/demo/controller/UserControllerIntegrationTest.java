530bab39d118f58fb4c8e09d35414436113d13a2
pcarson <8025daab96b77ca618a282f9a5d6ec6ddd350805>
2023-04-29 13:09:00 +0200

    test tidy

--- src/test/java/com/example/demo/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/example/demo/controller/UserControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class UserControllerIntegrationTest {
 
     @Autowired
